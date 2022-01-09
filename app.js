b2e51918a3472d98e5e7c10b9aa6b9efdb62b968
Zeldown <42982613+Zeldown@users.noreply.github.com>
2022-01-09 22:00:54 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,51 @@
+let url = 'https://github.com/Zeldown/BombParty/blob/main/words.json';
+
+let json;
+let interval;
+
+fetch(url).then(res => res.json()).then(out => {
+
+    json = out
+
+    console.log("Loaded json with " + json['length'] + " words");
+
+    let selfTurn = document.querySelector('.selfTurn');
+
+    let stopButton = document.createElement('button');
+    stopButton.innerHTML = "Stop"
+    stopButton.onclick = () => {
+        if(interval) {
+            clearInterval(interval);
+            stopButton.innerHTML = "Start";
+            interval = null;
+        }else {
+            start(selfTurn.children[0].children[0]);
+            stopButton.innerHTML = "Stop";
+        }
+    }
+
+    selfTurn.appendChild(stopButton);
+    
+    start(selfTurn.children[0].children[0]);
+});
+
+function start(input) {
+    interval = setInterval(function() {
+        let text = document.querySelector('.syllable').innerHTML;
+    
+        let foundWord;
+        for(let word of json) {
+            if(word.label.includes(text)) {
+                console.log("found word: " + word.label);
+                foundWord = word.label;
+                break;
+            }
+        }
+
+        if(foundWord) {
+            input.value = foundWord;
+        }else {
+            input.value = "Not found";
+        }
+    }, 1000);
+}
